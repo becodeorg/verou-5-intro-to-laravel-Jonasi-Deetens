92bd40f710bfc16fd5df283b9dcaf20b41dad661
Jonasi-Deetens <jonasideetens@hotmail.com>
2024-01-22 11:57:49 +0100

    Add named routes that link to eachother

---
+++ resources/views/second.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="{{ route('first') }}">First</a>
+    <h1>Second page!</h1>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel</title>
+</head>
+<body>
+    <a href="{{ route('first') }}">First</a>
+    <a href="{{ route('second') }}">Second</a>
+    <h1>Hello World!</h1>
+</body>
+</html>
